98d7726394866abe2112e3294e4304924fce1de5
LucasMontenegro1 <lu.montenegro98@gmail.com>
2023-03-24 20:40:20 -0300

    ADD: Qualifable and Sellable interfaces

---
+++ src/main/java/ar/uba/fi/tdd/exercise/Qualifable.java
@@ -0,0 +1,5 @@
+package ar.uba.fi.tdd.exercise;
+
+public interface Qualifable {
+    public abstract void updateQuality();
+}
---
+++ src/main/java/ar/uba/fi/tdd/exercise/Sellable.java
@@ -0,0 +1,5 @@
+package ar.uba.fi.tdd.exercise;
+
+public interface Sellable {
+    public abstract void updateSellIn();
+}
