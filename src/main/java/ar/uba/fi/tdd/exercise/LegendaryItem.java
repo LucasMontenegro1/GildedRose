2836c6a43ccb0a89a376032514246e4901a485ff
LucasMontenegro1 <lu.montenegro98@gmail.com>
2023-03-24 22:56:08 -0300

    ADD: LegendaryItem class

---
+++ src/main/java/ar/uba/fi/tdd/exercise/LegendaryItem.java
@@ -0,0 +1,16 @@
+package ar.uba.fi.tdd.exercise;
+
+public abstract class LegendaryItem implements Qualifable,Sellable{
+    protected Item item;
+    private static final int MAX_QUALITY = 80;
+
+    public LegendaryItem(Item item){
+        this.item = item;
+    }
+    //Doesn´t do anything since is a legendary item
+    @Override
+    public void updateQuality() {}
+
+    @Override
+    public void updateSellIn() {}
+}
