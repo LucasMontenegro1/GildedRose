3e88f378537546b9818443da4ef28bfd78978ed1
LucasMontenegro1 <lu.montenegro98@gmail.com>
2023-03-25 19:19:26 -0300

    ADD : BackStagePass, fix updateQualityMethod less than 8 lines

--- src/main/java/ar/uba/fi/tdd/exercise/StaleItem.java
+++ src/main/java/ar/uba/fi/tdd/exercise/StaleItem.java
@@ -1,7 +1,6 @@
 package ar.uba.fi.tdd.exercise;
 
 public class StaleItem implements Qualifiable {
-    private static final int DEGRADATION_RATE = 2;
     private static final int MAX_QUALITY = 50;
 
     private static final int SELL_IN_DATE = 0;
