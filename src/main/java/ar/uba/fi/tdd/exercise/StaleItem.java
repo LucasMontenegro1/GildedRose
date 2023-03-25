c3b0f1d017312a3226a986e756973ece9086a002
LucasMontenegro1 <lu.montenegro98@gmail.com>
2023-03-25 11:34:37 -0300

    ADD: StaleItems Class

---
+++ src/main/java/ar/uba/fi/tdd/exercise/StaleItem.java
@@ -0,0 +1,17 @@
+package ar.uba.fi.tdd.exercise;
+
+public class StaleItem implements Qualifable,Sellable{
+    public StaleItem(){
+
+    }
+
+    @Override
+    public void updateQuality() {
+
+    }
+
+    @Override
+    public void updateSellIn() {
+
+    }
+}
