2aba03957256e9307e36e4fa8a277c97309509b6
LucasMontenegro1 <lu.montenegro98@gmail.com>
2023-03-28 00:19:03 -0300

    ADD : tests

--- src/test/java/ar/uba/fi/tdd/exercise/GildedRoseTest.java
+++ src/test/java/ar/uba/fi/tdd/exercise/GildedRoseTest.java
@@ -3,6 +3,7 @@ package ar.uba.fi.tdd.exercise;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.*;
@@ -23,27 +24,86 @@ class GildedRoseTest {
 	}
 
 	@Test
-	public void SulfurasDoesNotDegrade(){
-		Item item = new Item("Sulfuras",10,80);
-		Sulfuras sulfuras = new Sulfuras(item);
-		sulfuras.updateQuality();
-		sulfuras.updateSellIn();
-		assertThat(item.quality).isEqualTo(80);
-		assertThat(item.sellIn).isEqualTo(10);
+	public void normalItemDegradesCorrectlyWhenSellInGreaterThanZero(){
+		Item item = new Item("Unknown",2,2);
+		Qualifiable normal = new NormalItem(item);
+		normal.updateSellIn();
+		normal.updateQuality();
+		assertThat(item.quality).isEqualTo(1);
 	}
 
 	@Test
-	public void AgedBrieHasMoreQuality(){
+	public void normalItemDegradesTwiceAsFastAfterSellIn(){
+		Item item = new Item("Unknown",0,2);
+		Qualifiable normal = new NormalItem(item);
+		normal.updateSellIn();
+		normal.updateQuality();
+		assertThat(item.quality).isEqualTo(0);
+	}
+
+	@Test
+	void qualityOfAnItemIsNeverNegative(){
+		Item item = new Item("Unknown",0,0);
+		Qualifiable normal = new NormalItem(item);
+		normal.updateSellIn();
+		normal.updateQuality();
+		assertThat(item.quality).isEqualTo(0);
+	}
+
+	@Test
+	public void cantCreateNormalItemWithQualityOver50(){
+		Item item = new Item("Unknown", 2,51);
+		Assertions.assertThrows(IllegalArgumentException.class, ()-> new NormalItem(item));
+	}
+
+	@Test
+	public void agedBrieHasMoreQuality(){
+		Item item = new Item("Aged Brie", 20,20);
+		AgedBrie brie = new AgedBrie(item);
+		brie.updateSellIn();
+		brie.updateQuality();
+		assertThat(item.quality).isEqualTo(21);
+	}
+
+	@Test
+	public void agedBrieHasLessSellIn(){
 		Item item = new Item("Aged Brie", 20,20);
 		AgedBrie brie = new AgedBrie(item);
 		brie.updateSellIn();
 		brie.updateQuality();
 		assertThat(item.sellIn).isEqualTo(19);
-		assertThat(item.quality).isEqualTo(21);
 	}
 
 	@Test
-	public void BackStagePassOfTenDays(){
+	public void agedBrieUpgradesCorrectlyTillMax(){
+		Item item = new Item("Aged Brie", 2,50);
+		ItemMaker maker = new ItemMaker();
+		Qualifiable brie = maker.makeItem(item);
+		brie.updateSellIn();
+		brie.updateQuality();
+		assertThat(item.quality).isEqualTo(50);
+	}
+
+	@Test
+	public void sulfurasDoesNotDegrade(){
+		Item item = new Item("Sulfuras",10,80);
+		Sulfuras sulfuras = new Sulfuras(item);
+		sulfuras.updateSellIn();
+		sulfuras.updateQuality();
+		assertThat(item.quality).isEqualTo(80);
+
+	}
+
+	@Test
+	public void sulfurasDoesNotHaveToBeSold(){
+		Item item = new Item("Sulfuras",10,80);
+		Sulfuras sulfuras = new Sulfuras(item);
+		sulfuras.updateSellIn();
+		assertThat(item.sellIn).isEqualTo(10);
+	}
+
+	@Test
+	public void backStagePassOfTenDays(){
 		Item item = new Item("Backstage Pass", 10,20);
 		BackStagePass back = new BackStagePass(item);
 		back.updateSellIn();
@@ -52,7 +112,7 @@ class GildedRoseTest {
 	}
 
 	@Test
-	public void BackStagePassOfFiveDays(){
+	public void backStagePassOfFiveDays(){
 		Item item = new Item("Backstage Pass", 5,20);
 		BackStagePass back = new BackStagePass(item);
 		back.updateSellIn();
@@ -61,7 +121,7 @@ class GildedRoseTest {
 	}
 
 	@Test
-	public void BackStageWhenDayBecomesZero(){
+	public void backStageWhenDayBecomesZero(){
 		Item item = new Item("Backstage Pass", 1,20);
 		BackStagePass back = new BackStagePass(item);
 		back.updateSellIn();
@@ -78,23 +138,29 @@ class GildedRoseTest {
 	}
 
 	@Test
-	public void agedBrieUpgradesCorrectlyTillMax(){
-		Item item = new Item("Aged Brie", 2,50);
+	public void conjuredItemDegradesTwiceAsFastNormally(){
+		Item item = new Item("Conjured Mana Cake", 2,20);
 		ItemMaker maker = new ItemMaker();
-		Qualifiable brie = maker.makeItem(item);
-		brie.updateSellIn();
-		brie.updateQuality();
-		assertThat(item.quality).isEqualTo(50);
+		Qualifiable cake = maker.makeItem(item);
+		cake.updateSellIn();
+		cake.updateQuality();
+		assertThat(item.quality).isEqualTo(18);
 	}
 
 	@Test
-	public void normalItemCantGetLessThanZero(){
-		Item item = new Item("Normal Item", 2,0);
+	public void conjuredItemDegradesTwiceAsFastAfterZero(){
+		Item item = new Item("Conjured Mana Cake", 0,20);
 		ItemMaker maker = new ItemMaker();
-		Qualifiable brie = maker.makeItem(item);
-		brie.updateSellIn();
-		brie.updateQuality();
-		assertThat(item.quality).isEqualTo(0);
+		Qualifiable cake = maker.makeItem(item);
+		cake.updateSellIn();
+		cake.updateQuality();
+		assertThat(item.quality).isEqualTo(16);
+	}
+
+	@Test
+	public void legendaryItemsQualityShouldBe80(){
+		Item item = new Item("Sulfuras, Hand of Ragnaros",10,79);
+		Assertions.assertThrows(IllegalArgumentException.class, ()-> new Sulfuras(item));
 	}
 
 }
